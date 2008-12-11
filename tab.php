52d79aff6258c21fe871e41e25aca6923cedbc9b
Chris Pleasants <cpflames@cpflames-desktop.(none)>
2008-12-10 22:22:36 -0800

    updated tab

--- tab.php
+++ tab.php
@@ -1,1 +1,11 @@
-It's a tab!
+<h1>Pass the pigs!</h1>
+
+<h2>Existing games</h2>
+
+<h2>Create a new game</h2>
+<p>Invite your friends to a game!</p>
+<form>
+<p>Friend 1: <fb:friend-selector /></p>
+<p>Friend 2: <fb:friend-selector /></p>
+<p>Friend 3: <fb:friend-selector /></p>
+</form>
